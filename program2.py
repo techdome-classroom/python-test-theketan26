db14eb4674d58977170152efa5dd8b716026764a
theketan26 <ketangsolanki129@gmail.com>
2024-01-04 23:46:42 -0800

    program 2 completed

--- program2.py
+++ program2.py
@@ -1,7 +1,68 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        i = 0
+        num = 0
+        while i < len(s):
+            if s[i] == 'M':
+                num += 1000
+                i += 1
+
+            elif s[i] == 'C':
+                if i < len(s) - 1:
+                    if s[i + 1] == 'M':
+                        num += 900
+                        i += 2
+                    elif s[i + 1] == 'D':
+                        num += 400
+                        i += 2
+                    else:
+                        num += 100
+                        i += 1
+                else:
+                    num += 100
+                    i += 1
+
+            elif s[i] == 'X':
+                if i < len(s) - 1:
+                    if s[i + 1] == 'C':
+                        num += 90
+                        i += 2
+                    elif s[i + 1] == 'L':
+                        num += 40
+                        i += 2
+                    else:
+                        num += 10
+                        i += 1
+                else:
+                    num += 10
+                    i += 1
+            
+            elif s[i] == 'I':
+                if i < len(s) - 1:
+                    if s[i + 1] == 'X':
+                        num += 9
+                        i += 2
+                    elif s[i + 1] == 'V':
+                        num += 4
+                        i += 2
+                    else:
+                        num += 1
+                        i += 1
+                else:
+                    num += 1
+                    i += 1
+
+            elif s[i] == 'D':
+                num += 500
+                i += 1
+            
+            elif s[i] == 'L':
+                num += 50
+                i += 1
+            
+            elif s[i] == 'V':
+                num += 5
+                i += 1
+            
+        return num
+
