f542e1e7bd9e9f9f45b3b7ff5526ee009f06f9a8
theketan26 <ketangsolanki129@gmail.com>
2024-01-04 23:30:01 -0800

    program 1 completed

--- program1.py
+++ program1.py
@@ -1,13 +1,29 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        valid = False
+        queue = []
+        for c in s:
+            if c in ['(', '{', '[']:
+                queue += c
+            elif c in [')']:
+                if queue[-1] == '(':
+                    queue.pop()
+                else:
+                    break
+            elif c in ['}']:
+                if queue[-1] == '{':
+                    queue.pop()
+                else:
+                    break
+            elif c in [']']:
+                if queue[-1] == '[':
+                    queue.pop()
+                else:
+                    break
+
+        if queue == []:
+            valid = True
+
+        return valid
     
-
-
-
-  
 
